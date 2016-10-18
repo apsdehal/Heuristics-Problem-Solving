28686ed567679d53f386024878fbf732b4c268fd
Simranjyot Singh <simranjyot1991@gmail.com>
2016-10-18 05:38:50 -0400

    hanges in coordinateList file

--- week5/coordinateList.py
+++ week5/coordinateList.py
@@ -1,7 +1,11 @@
 import math
 
+myCoordinateList = []
+
 def getCoordinateList():
-	myCoordinateList = []
+	global myCoordinateList
+	if(len(myCoordinateList)!=0):
+		return myCoordinateList
 	xPrev = -1
 	for y in range(0,67):
 		x = math.ceil(math.sqrt(4356 - pow(y,2)))
@@ -9,4 +13,20 @@ def getCoordinateList():
 		if(x!=xPrev):
 			myCoordinateList.append((x,y))
 		xPrev = x;
+	for pair in reversed(myCoordinateList):
+		x = pair[0]* -1
+		y = pair[1]
+		if(x!=0):
+			myCoordinateList.append((x,y))
+	
+	for pair in myCoordinateList[:]:
+		x = pair[0]
+		y = pair[1] * -1
+		if(y!=0):
+			myCoordinateList.append((x,y))
+
 	return myCoordinateList
+
+m = getCoordinateList()
+for a in m:
+	print(a)
