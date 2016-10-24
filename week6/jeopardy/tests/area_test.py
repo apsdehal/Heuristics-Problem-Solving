2d567abff8bff43a3cc19edf16eeb95c7d194d39
Amanpreet Singh <apsdehal@gmail.com>
2016-10-24 10:00:41 -0400

    Add tests for area

---
+++ week6/jeopardy/tests/area_test.py
@@ -0,0 +1,23 @@
+from helpers.hunter import Hunter
+from helpers.coordinate import Coordinate
+from helpers.wall import Wall
+
+if __name__ == '__main__':
+
+    info = {}
+    info['maxWalls'] = 1
+    info['wallPlacementDelay'] = 1
+    info['boardSizeX'] = 300
+    info['boardSizeY'] = 300
+    info['walls'] = []
+    info['walls'].append(Wall(0, 100, 100, 300))
+    info['walls'].append(Wall(0, 150, 100, 300))
+    info['walls'].append(Wall(1, 100, 100, 300))
+    info['walls'].append(Wall(1, 150, 100, 300))
+    info['walls'].append(Wall(1, 250, 100, 300))
+
+    hunter = Hunter(info)
+    hunter.walls = info['walls']
+    hunter.preyCoord = Coordinate(230, 200)
+
+    print hunter.preyArea()
