4596923b83af992549615ead0765314e0ba38a47
Amanpreet Singh <apsdehal@gmail.com>
2016-10-24 07:05:33 -0400

    Add occupies function to board

--- week6/jeopardy/helpers/wall.py
+++ week6/jeopardy/helpers/wall.py
@@ -7,3 +7,12 @@ class Wall:
         self.coord = coord
         self.start = start
         self.end = end
+
+    def occupies(self, coord):
+        if self.type == HORIZONTAL:
+            return coord.y == self.coord.y and self.coord.start <= coord.x and self.coord.end >= coord.x
+        elif self.type == VERTICAL:
+            return coord.x == self.coord.x and self.coord.start <= coord.y and self.coord.end >= coord.y
+
+        # We should not be here technically, just in case
+        return 0
