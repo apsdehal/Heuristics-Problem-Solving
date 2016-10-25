b03d809c11e6e1be02574fd30a5ff1c002081cc0
Simranjyot Singh <simranjyot1991@gmail.com>
2016-10-24 21:18:23 -0400

    greedy stratergy for prey

--- week6/jeopardy/helpers/prey.py
+++ week6/jeopardy/helpers/prey.py
@@ -1,2 +1,14 @@
+from coordinate import Coordinate
 class Prey:
-    def __init__(self):
+    def __init__(self,info):
+    	self.x_coord = info['prey'].x 
+    	self.y_coord = info['prey'].y
+
+    def move(self,info):
+        resp['gameNum'] = info['gameNum']
+        resp['tickNum'] = info['tickNum']
+
+        resp['x'] = self.x_coord + info['hunter'].vx
+        resp['y'] = self.y_coord + info['hunter'].vy
+
+        return resp
