7a517f3b4f9e03ba5deb7246cc06845a0dc294c5
Amanpreet Singh <apsdehal@gmail.com>
2016-10-31 22:08:06 -0400

    Parse nStars as argument

--- week7/helpers/choreo.py
+++ week7/helpers/choreo.py
@@ -3,6 +3,6 @@ from util import getPairs
 
 class Choreo:
     def __init__(self, io):
-
+        None
     def move(reds,blues):
         pairs = getPairs(reds,blues)
--- week7/helpers/spoiler.py
+++ week7/helpers/spoiler.py
@@ -1,2 +1,3 @@
 class Spoiler:
     def __init__(self, io):
+        self.stars = io.nStars
