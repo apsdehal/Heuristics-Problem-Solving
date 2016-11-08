22b7452e24aa5426d2b17ef96f176118871232cf
Simranjyot Singh <simranjyot1991@gmail.com>
2016-11-08 06:20:24 -0500

    fixing bug when prey and hunter are moving closer and on same diagnal

--- week6/jeopardy/helpers/prey.py
+++ week6/jeopardy/helpers/prey.py
@@ -7,6 +7,8 @@ class Prey:
         self.y_coord = info['prey'].y
         self.hunter_x = info['hunter'].x
         self.hunter_y = info['hunter'].y
+        self.hunter_vx = info['hunter'].vx
+        self.hunter_vy = info['hunter'].vy
 
 
     def move(self,info):
@@ -14,6 +16,8 @@ class Prey:
         self.y_coord = info['prey'].y
         self.hunter_x = info['hunter'].x
         self.hunter_y = info['hunter'].y
+        self.hunter_vx = info['hunter'].vx
+        self.hunter_vy = info['hunter'].vy
         resp = {}
         resp['gameNum'] = info['gameNum']
         resp['tickNum'] = info['tickNum']
@@ -22,8 +26,8 @@ class Prey:
             resp['y'] = 0
             return resp
         dist = math.pow(self.x_coord - self.hunter_x,2) + math.pow(self.y_coord -self.hunter_y ,2)
-        if(dist>100):
-            #100 because I am not taking sqaure root
+        if(dist>225):
+            #225 because I am not taking sqaure root
             if(self.hunter_x > self.x_coord):
                 resp['x'] = 1
             else:
@@ -34,13 +38,19 @@ class Prey:
                 resp['y'] = -1
             return resp
         else:
-            if(self.x_coord < self.hunter_x):
-                resp['x'] = - 1
+            x_dist = abs(self.hunter_x-self.x_coord)
+            y_dist = abs(self.hunter_y-self.y_coord)
+            x_dist_future = abs(self.hunter_x + self.hunter_vx -self.x_coord)
+            y_dist_future = abs(self.hunter_y + self.hunter_vy -self.y_coord)
+            if(x_dist_future < x_dist):
+                resp['x'] = self.hunter_vx
+                resp['y'] = 0-self.hunter_vy
+            elif(y_dist_future<y_dist):
+                resp['x'] = 0-self.hunter_vx
+                resp['y'] = self.hunter_vy
             else:
-                resp['x'] = 1
-            if(self.y_coord < self.hunter_y):
-                resp['y'] = 1 
-            else:
-                resp['y'] = - 1
+                resp['x'] = 0
+                resp['y'] = 0
+            #print dist, " vx:", self.hunter_vx, " vy:", self.hunter_vy, " resp:", resp['x'], ",", resp['y']
             return resp
 
