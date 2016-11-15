2aa8fef6c9eafb3b5d0300121b1cdf92a3ef504f
Amanpreet Singh <apsdehal@gmail.com>
2016-11-15 00:37:10 -0500

    Complete person io

--- week9/jeopardy/helpers/io.py
+++ week9/jeopardy/helpers/io.py
@@ -1,5 +1,42 @@
 from person import Person
 from matchmaker import Matchmaker
+import socket
 
 class IO:
     def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(('localhost', PORT))
+
+    def start(self, playerName):
+        numString = self.sock.recv(4)
+        assert numString.endswith('\n')
+
+        numAttr = int(numString[:-1])
+        self.numAttr = numAttr
+
+        if playerName == 'person':
+            self.player = Person(numAttr)
+            self.startPersonIO()
+        elif playerName == 'matchmaker':
+            self.player = Matchmaker(numAttr)
+            self.startMatchmakerIO()
+
+        self.sock.close()
+
+    def startPersonIO(self):
+        initialWeights = self.player.getValidWeights()
+        idealCandidate = self.player.getIdealCandidate()
+        antiIdealCandidate = self.player.getAntiIdealCandidate()
+
+        self.sock.sendall(utils.floats_to_msg2(initialWeights))
+
+        self.sock.sendall(utils.candidate_to_msg(idealCandidate))
+        self.sock.sendall(utils.candidate_to_msg(antiIdealCandidate))
+
+
+        for i in range(20):
+            # 7 char weights + commas + exclamation
+            data = sock.recv(8*num_attr)
+            print('%d: Received guess = %r' % (i, data))
+            assert data[-1] == '\n'
+            sock.send(floats_to_msg2(self.player.getNewWeights(data)))
--- week9/jeopardy/helpers/person.py
+++ week9/jeopardy/helpers/person.py
@@ -1,3 +1,20 @@
 class Person:
-    def __init__(self):
-        return
+    def __init__(self, numAttr):
+        self.numAttr = numAttr
+        self.initialWeights = []
+
+    def getValidWeights(self):
+        # Set initial weight here
+
+    def getIdealCandidate(self):
+        # Initial setup for ideal candidate, change accordingly
+        return self.initialWeights > 0
+
+    def getAntiIdealCandidate(self):
+        # Initial setup for anti ideal candidate, change accordingly
+        return self.initialWeights <= 0
+
+    def getNewWeights(self, guess):
+        # Use guess and self.initialWeights to calculate new data
+        # send back initial weight for now
+        return self.initialWeights
